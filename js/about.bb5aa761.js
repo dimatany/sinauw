8b6bbb91ea64d42767327ee5676603a20cca5aa7
Dima Tatiana <Cheize2020@gmail.com>
2022-07-25 20:12:34 +0300

    initial commit

---
+++ js/about.bb5aa761.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksinauw"]=self["webpackChunksinauw"]||[]).push([[443],{9983:function(n,u,r){r.r(u),r.d(u,{default:function(){return o}});var s=r(3396);const t={class:"about"},a=(0,s._)("h1",null,null,-1),e=[a];function c(n,u){return(0,s.wg)(),(0,s.iD)("div",t,e)}var i=r(89);const l={},f=(0,i.Z)(l,[["render",c]]);var o=f}}]);
+//# sourceMappingURL=about.bb5aa761.js.map
