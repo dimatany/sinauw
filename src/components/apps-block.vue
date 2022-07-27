d7b5dc49d75886f7767f611e867c05d85e0acca8
Dima Tatiana <Cheize2020@gmail.com>
2022-07-27 22:56:17 +0300

    changes

--- src/components/apps-block.vue
+++ src/components/apps-block.vue
@@ -111,8 +111,6 @@ $border-color: rgba(154, 156, 203, 0.6);
   overflow: hidden;
   animation: hue 3s infinite alternate;
   animation-delay: 0s;
-  -webkit-animation: hue 3s infinite alternate;
-  -webkit-animation-delay: 0s;
   .apps-info {
     padding-top: 16px;
     padding-left: 31px;
@@ -348,33 +346,16 @@ $border-color: rgba(154, 156, 203, 0.6);
   position: absolute;
   top: 0;
   left: 0;
-  -webkit-filter: blur(3rem);
   filter: blur(3rem);
   z-index: -1;
-  -webkit-transform: scale(2);
-  -ms-transform: scale(2);
   transform:scale(2)
 }
-@-webkit-keyframes hue {
-  from {
-    -webkit-filter: hue-rotate(-30deg);
-    filter:hue-rotate(-30deg)
-  }
-
-  to {
-    -webkit-filter: hue-rotate(10deg);
-    filter:hue-rotate(10deg)
-  }
-}
-
 @keyframes hue {
   from {
-    -webkit-filter: hue-rotate(-30deg);
     filter:hue-rotate(-30deg)
   }
 
   to {
-    -webkit-filter: hue-rotate(10deg);
     filter:hue-rotate(10deg)
   }
 }
